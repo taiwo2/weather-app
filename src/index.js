5420ab02c3f624e1ba08c9f0edc8633e36d1e120
taiwo2 <adetonat@ymail.com>
2021-04-23 17:26:03 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,50 +1,47 @@
-var cityDisplay = document.getElementById("location");
-var temp = document.getElementById("temp");
-var icon = document.getElementById("icon");
-var des = document.getElementById("des");
-var humidity = document.getElementById("humidity");
-var windspeed = document.getElementById("windspeed");
-var form = document.querySelector("form");
-var formInput = document.getElementById("searchbar");
-var unitBtn = document.getElementById("unit-control");
-var warning = document.getElementById("warning");
+const cityDisplay = document.getElementById('location');
+const temp = document.getElementById('temp');
+const icon = document.getElementById('icon');
+const des = document.getElementById('des');
+const humidity = document.getElementById('humidity');
+const windspeed = document.getElementById('windspeed');
+const form = document.querySelector('form');
+const formInput = document.getElementById('searchbar');
+const unitBtn = document.getElementById('unit-control');
+const warning = document.getElementById('warning')
+const cityName =  cityDisplay.innerText.substr(0, cityDisplay.innerText.length-3);
+const windUnit;
 
-var cityName =  cityDisplay.innerText.substr(0, cityDisplay.innerText.length-3);
-
-var windUnit;
-
-async function getWeather(city="mumbai", unit="°C"){
+async function getWeather(city='lagos', unit='°C'){
   try{
       var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=610141c99f975bba14f72033833cff4e`, {mode: "cors"});
       var data = await response.json();
       renderInfo(data, unit);
   } catch(err){
-      warning.classList.add("show")
+      warning.classList.add('show')
       setTimeout(()=>{
-          warning.classList.remove("show");
+          warning.classList.remove('show');
       }, 1500)
       renderInfo(cityName, unit)
   }
-    
 }
 
-window.addEventListener("load", ()=>{
+window.addEventListener('load', ()=>{
     getWeather();
 })
 
-form.addEventListener("submit", (e)=>{
+form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    var check = unitBtn.innerText=="°C"? "°F": "°C";
+    var check = unitBtn.innerText=='°C'? '°F': '°C';
     getWeather(formInput.value, check);
-    formInput.value = "";
+    formInput.value = '';
 })
-unitBtn.addEventListener("click", ()=>{
+unitBtn.addEventListener('click', ()=>{
     cityName =  cityDisplay.innerText.substr(0, cityDisplay.innerText.length-3);
     getWeather(cityName, unitBtn.innerText)
-    if(unitBtn.innerText == "°F"){
-        unitBtn.innerText = "°C";
+    if(unitBtn.innerText == '°F'){
+        unitBtn.innerText = '°C';
     } else{
-        unitBtn.innerText = "°F";
+        unitBtn.innerText = '°F';
     }
 })
 
@@ -53,10 +50,10 @@ function renderInfo(arg, unit){
     temp.innerText = `${tempConversion(unit, arg)} ${unit}`;
     des.innerText = arg.weather[0].main;
     icon.style.background = `url(https://openweathermap.org/img/wn/${arg.weather[0].icon}@2x.png) center no-repeat, linear-gradient(rgba(255, 255, 255, 0.5),rgba(255, 255, 255, 0.5))`;
-    windspeed.innerText = arg.wind.speed + " mph";
-    humidity.innerText = arg.main.humidity + " %";
+    windspeed.innerText = arg.wind.speed + 'mph';
+    humidity.innerText = arg.main.humidity + '%';
 }
 
 function tempConversion(unit, arg){
-    return unit=="°C"? Math.round(arg.main.temp) : Math.round(arg.main.temp * (9/5)) + 32;
+    return unit=='°C'? Math.round(arg.main.temp) : Math.round(arg.main.temp * (9/5)) + 32;
 }
