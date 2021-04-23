26724dab199782ac89f5fcfe82f1e2b01248f913
taiwo2 <adetonat@ymail.com>
2021-04-23 17:05:16 +0100

    Add index.js

--- src/index.js
+++ src/index.js
@@ -9,102 +9,54 @@ var formInput = document.getElementById("searchbar");
 var unitBtn = document.getElementById("unit-control");
 var warning = document.getElementById("warning");
 
-  const conditions = [
-    {
-      condition: 'Thunderstorm',
-      icon: '<i class="fas fa-thunderstorm"></i>',
-    },
-    {
-      condition: 'Drizzle',
-      icon: '<i class="fas fa-cloud-rain"></i>',
-    },
-    {
-      condition: 'Rain',
-      icon: '<i class="fas fa-cloud-showers-heavy"></i>',
-    },
-    {
-      condition: 'Snow',
-      icon: '<i class="fas fa-snowflake"></i>',
-    },
-    {
-      condition: 'Clear',
-      icon: '<i class="fas fa-sun"></i>',
-    },
-    {
-      condition: 'Clouds',
-      icon: '<i class="fas fa-cloud"></i>',
-    },
-    {
-      condition: 'Mist',
-      icon: '<i class="fas fa-cloud-sun"></i>',
-    },
-  ];
+var cityName =  cityDisplay.innerText.substr(0, cityDisplay.innerText.length-3);
 
-  const inputCity = document.getElementById('input-city');
-  const invalidCityText = document.getElementById('invalid-city-text');
-  const form = document.getElementById('form');
-  const convert = (temp) => {
-    const formula = parseInt(((temp - 273.15) * 9) / 5 + 32, 10);
-    return formula;
-  };
+var windUnit;
 
-  const populate = (data) => {
-    const cityName = document.getElementById('city-name');
-    const weatherIcon = document.getElementById('weather-icon');
-    const temperature = document.getElementById('temperature');
-    const weatherText = document.getElementById('weather-text');
-    const humidity = document.getElementById('humidity');
-    const winds = document.getElementById('winds');
-
-    cityName.textContent = data.name;
-    temperature.textContent = `${convert(data.main.temp)}°`;
-    weatherText.textContent = data.weather[0].main;
-    humidity.textContent = `Humidity: ${data.main.humidity}%`;
-    winds.textContent = `Winds: ${data.wind.speed} meter/sec`;
-
-    for (let i = 0; i < conditions.length; i += 1) {
-      if (conditions[i].condition === data.weather[0].main) {
-        weatherIcon.innerHTML = conditions[i].icon;
-      }
+async function getWeather(city="mumbai", unit="°C"){
+    try{
+        var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=610141c99f975bba14f72033833cff4e`, {mode: "cors"});
+        var data = await response.json();
+        renderInfo(data, unit);
+    } catch(err){
+        warning.classList.add("show")
+        setTimeout(()=>{
+            warning.classList.remove("show");
+        }, 1500)
+        renderInfo(cityName, unit)
     }
-  };
+    
+}
 
-  // Fetch api data
-  async function apiCall() {
-    try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&appid=8d1c03ca974d24a85e482dd06b1f0a63
-      `, { mode: 'cors' },
-      );
-      if (response.status === 404) {
-        invalidCityText.style.display = 'block';
-      } else {
-        const weatherData = await response.json().then((data) => populate(data));
-        invalidCityText.style.display = 'none';
-        return weatherData;
-      }
-    } catch (e) {
-      console.log(e);
+window.addEventListener("load", ()=>{
+    getWeather();
+})
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    var check = unitBtn.innerText=="°C"? "°F": "°C";
+    getWeather(formInput.value, check);
+    formInput.value = "";
+})
+unitBtn.addEventListener("click", ()=>{
+    cityName =  cityDisplay.innerText.substr(0, cityDisplay.innerText.length-3);
+    getWeather(cityName, unitBtn.innerText)
+    if(unitBtn.innerText == "°F"){
+        unitBtn.innerText = "°C";
+    } else{
+        unitBtn.innerText = "°F";
     }
-    return null;
-  }
+})
 
-  form.addEventListener('submit', apiCall);
-  // Initialize los angeles weather by default
-  async function init(city) {
-    try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8d1c03ca974d24a85e482dd06b1f0a63
-      `, { mode: 'cors' },
-      );
-      const weatherData = await response.json().then((data) => populate(data));
-      return weatherData;
-    } catch (e) {
-      console.log(e);
-    }
-    return null;
-  }
+function renderInfo(arg, unit){
+    cityDisplay.innerText = `${arg.name}, ${arg.sys.country}`; 
+    temp.innerText = `${tempConversion(unit, arg)} ${unit}`;
+    des.innerText = arg.weather[0].main;
+    icon.style.background = `url(https://openweathermap.org/img/wn/${arg.weather[0].icon}@2x.png) center no-repeat, linear-gradient(rgba(255, 255, 255, 0.5),rgba(255, 255, 255, 0.5))`;
+    windspeed.innerText = arg.wind.speed + " mph";
+    humidity.innerText = arg.main.humidity + " %";
+}
 
-  init('lagos');
-})();
-displayController();
+function tempConversion(unit, arg){
+    return unit=="°C"? Math.round(arg.main.temp) : Math.round(arg.main.temp * (9/5)) + 32;
+}
